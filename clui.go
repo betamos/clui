4f0a765843da4ffe5ffde61a5c784db0b2f918ff
Didrik Nordström <didrik@betamos.se>
2017-10-24 10:39:17 -0700

    Multiple tasks wip

--- clui.go
+++ clui.go
@@ -7,86 +7,88 @@ import (
 	"time"
 )
 
-type Spinner struct {
-	theme     []string
-	tickSpeed time.Duration
-	tickCount int
-	status    string // enum?
-	message   string
-	ticker    *time.Ticker
-	mtx       *sync.Mutex
+type Task struct {
+	theme   []string
+	status  string // enum?
+	message string
+	mtx     *sync.Mutex
 }
 
-func NewSpinner(message string) *Spinner {
-	s := &Spinner{
-		theme:     []string{"⠛", "⠹", "⢸", "⣰", "⣤", "⣆", "⡇", "⠏"},
-		tickSpeed: 150 * time.Millisecond,
-		tickCount: 0,
-		status:    "working",
-		message:   message,
-		mtx:       &sync.Mutex{},
+var (
+	tasks     []*Task
+	mtx       *sync.Mutex = &sync.Mutex{}
+	ticker    *time.Ticker
+	tickSpeed time.Duration = 150 * time.Millisecond
+	tickCount int
+)
+
+func NewTask(message string) *Task {
+	s := &Task{
+		theme:   []string{"⠛", "⠹", "⢸", "⣰", "⣤", "⣆", "⡇", "⠏"},
+		status:  "working",
+		message: message,
+		mtx:     &sync.Mutex{},
 	}
+	tasks = append(tasks, s)
+	fmt.Println()
+	go ui()
 	return s
 }
 
-func (s *Spinner) redraw() {
-	s.mtx.Lock()
-	fmt.Print("\u001b[2K") // clear line
-	fmt.Print("\u001b[0G") // to column 0
-	if s.status == "success" {
-		color.Set(color.FgGreen)
-		fmt.Print("✔")
-		color.Unset()
-	} else if s.status == "fail" {
-		color.Set(color.FgRed)
-		fmt.Print("✘")
-		color.Unset()
+func redraw() {
+	mtx.Lock()
 
-	} else {
-		color.Set(color.FgYellow)
-		fmt.Print(s.theme[s.tickCount%len(s.theme)])
-		color.Unset()
-	}
-	fmt.Printf(" %v", s.message)
-	s.mtx.Unlock()
+	fmt.Print("\u001b[0G")               // to column 0
+	fmt.Printf("\u001b[%vA", len(tasks)) // to row -n
+	for _, t := range tasks {
+		fmt.Print("\u001b[2K") // clear line
+		fmt.Print("\u001b[0G") // to column 0
+		if t.status == "success" {
+			color.Set(color.FgGreen)
+			fmt.Print("✔")
+			color.Unset()
+		} else if t.status == "fail" {
+			color.Set(color.FgRed)
+			fmt.Print("✘")
+			color.Unset()
 
-}
-
-func (s *Spinner) ui() {
-	for {
-		select {
-		case <-s.ticker.C:
-			s.tickCount++
-			if s.status != "working" {
-				break
-			}
-			s.redraw()
+		} else {
+			color.Set(color.FgYellow)
+			fmt.Print(t.theme[tickCount%len(t.theme)])
+			color.Unset()
 		}
+		fmt.Printf(" %v\n", t.message)
+	}
+	mtx.Unlock()
+
+}
+
+func ui() {
+	if ticker != nil {
+		return
+	}
+	ticker = time.NewTicker(tickSpeed)
+	for range ticker.C {
+		tickCount++
+		redraw()
 	}
 }
 
-func (s *Spinner) Show() {
-	s.ticker = time.NewTicker(s.tickSpeed)
-	go s.ui()
+func (s *Task) Fail(message string) {
+	s.complete(message, "fail")
+}
+func (s *Task) Success(message string) {
+	s.complete(message, "success")
 }
 
-func (s *Spinner) Fail(message string) {
-	s.ticker.Stop()
+func (s *Task) complete(message, status string) {
+	//s.ticker.Stop()
 	s.message = message
-	s.status = "fail"
-	s.redraw()
-	fmt.Println()
+	s.status = status
+	redraw()
 }
 
-func (s *Spinner) Success(message string) {
-	s.ticker.Stop()
+func (s *Task) Update(message string) {
 	s.message = message
-	s.status = "success"
-	s.redraw()
-	fmt.Println()
-}
-
-func (s *Spinner) Update(message string) {
-	s.message = message
-	s.redraw()
+	redraw()
 }
--- wip.md
+++ wip.md
@@ -53,3 +53,41 @@ s1 := spinner.New("doin me a trick")
 s1.Warn("why oh why")
 
 
+
+
+
+# For general UI:
+
+clui.Colors = False
+
+clui.Theme = ...  // Sensible default
+
+clui.SuccessFile = os.Stdout
+clui.FailFile = os.Stderr
+clui.WarnFile = os.Stdout
+clui.UpdateFile = os.DevNull  // This is unnecessary
+
+# Done
+
+clui.Success("greeted user")
+clui.Fail("the file %v had the wrong permissions", f)
+clui.Warn("disk almost full")
+
+# Easy progress
+task := clui.NewTask("uploading files")
+// count to a million
+task.Success() // persists something hey
+
+# Multiple processes
+t1 := clui.NewTask("uploading files")
+// 1s of work
+t2 := clui.NewTask("provisioning VMs")
+// 2s of work
+t2.Fail("provisioning VMs failed")
+// 1s of work
+t1.Success("upload complete")  // stop displaying these things
+
+
+# internal data structure
+
+var []Task tasks
